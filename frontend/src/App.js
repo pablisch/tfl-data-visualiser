56ca3e43de844c03ff2264b20d4e0f69aa87b531
Pablo <pablito@me.com>
2023-06-03 14:23:06 +0100

    data visualiser complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,154 +1,53 @@
 import axios from 'axios';
-// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import TubeMap from './components/TubeMap/TubeMap.js';
-import * as Tone from 'tone';
-import assignNoteForVictoriaLine from './note-assignments/victoria-line'
-import assignNoteForJubileeLine from './note-assignments/jubilee-line'
-import assignNoteForNorthernLine from './note-assignments/northern-line'
 import DataVisualiser from './components/DataVisualiser/DataVisualiser.js';
 const { abridgeData, quantiseData } = require('./processTubeData');
-
 
 const dataBlockDuration = 30; // seconds between fetch from TFL
 
 function App() {
+  const [quantisedData, setQuantisedData] = useState([]);
 
-  var fade_state = true;
-  const fadeElement = (elementId) => {
-    const element = document.getElementById(elementId);
-    console.log(element.id);
-    if (fade_state === true) {
-      console.log("Fade out");
-      element.style.animation = "fade-out 1s forwards";
-      fade_state = false;
-    } else if (fade_state === false) {
-      console.log("Fade In");
-      element.style.animation = "fade-in 1s forwards";
-      fade_state = true;
-    }
-  }
-
-  // let durationPassed = 0; // base time used in Arrivals.js
-
-  const lines = "bakerloo,central,circle,district,hammersmith-city,jubilee,metropolitan,northern,piccadilly,victoria,waterloo-city";
-  let instruments = {}; // object to hold Tone instruments, intialised w global scope
-
-  const toneStart = async () => {
-    await Tone.start()
-
-    const freeverb = new Tone.Freeverb().toDestination();
-    freeverb.dampening = 1000;
-
-    const pizzViolaSampler = new Tone.Sampler({
-      urls: {
-        "A3": "viola_A3_1_piano_pizz-normal.mp3",
-        // "A1": "double-bass_A1_05_forte_arco-normal.mp3",
-      },
-      release: 1,
-      baseUrl: "/samples/",
-    }).connect(freeverb);
-    pizzViolaSampler.volume.value = -1
-
-    const frenchHornSampler = new Tone.Sampler({
-      urls: {
-        "C4": "french-horn_C4_15_piano_normal.mp3",
-      },
-      release: 1,
-      baseUrl: "/samples/",
-    }).connect(freeverb);
-    frenchHornSampler.volume.value = -1
-
-    const doubleBassSampler = new Tone.Sampler({
-      urls: {
-        "A1": "double-bass_A1_05_forte_arco-normal.mp3",
-      },
-      release: 1,
-      baseUrl: "/samples/",
-    });
-    doubleBassSampler.volume.value = 5
-    await Tone.loaded();
-
-    instruments = {
-      Victoria: pizzViolaSampler, 
-      Jubilee: frenchHornSampler,
-      Northern: doubleBassSampler
-    };
-  }
-
-  const fetchData = () => {
-    axios.get(`https://api.tfl.gov.uk/Line/${lines}/Arrivals?`)
-      .then(response => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const lines = "bakerloo,central,circle,district,hammersmith-city,jubilee,metropolitan,northern,piccadilly,victoria,waterloo-city";
+        const response = await axios.get(
+          `https://api.tfl.gov.uk/Line/${lines}/Arrivals?`
+        );
         const data = response.data;
         const filteredData = data
-          .filter(item => item.timeToStation < dataBlockDuration)
-          .map(item => ({
+          .filter((item) => item.timeToStation < dataBlockDuration)
+          .map((item) => ({
             id: item.id,
             stationName: item.stationName,
             lineName: item.lineName,
-            timeToStation: item.timeToStation
+            timeToStation: item.timeToStation,
           }));
-        const sortedData = filteredData.sort((a, b) => a.timeToStation - b.timeToStation);
+        const sortedData = filteredData.sort(
+          (a, b) => a.timeToStation - b.timeToStation
+        );
         const abridgedData = abridgeData(sortedData);
         const quantisedData = quantiseData(abridgedData);
-        // localStorage.setItem('quantisedData', JSON.stringify(quantisedData, null, 2)); // FOR DATA COLLECTION ONLY
+        setQuantisedData(quantisedData);
         console.log(quantisedData);
-        playSounds(quantisedData, instruments);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error fetching tube data:', error);
-      });
-  };
-
-  const soundOn = async () => {
-    await toneStart()
-    console.log('tone started')
-    fetchData(); // initial fetch as setInterval only exectues after first interval
-    setInterval(fetchData, dataBlockDuration * 1000);
-  }
-
-  const playSounds = (quantisedTubeData, instruments) => {
-    quantisedTubeData.forEach((train) => {
-      // can this be refactored into one expression for all lines?
-      if (train.lineName === 'Victoria') {
-        const note = assignNoteForVictoriaLine(train.stationName);
-        setTimeout(() => {
-          instruments.Victoria.triggerAttackRelease(note, '16n');
-          console.log(`${train.stationName} on the ${train.lineName} Time To Station: ${train.timeToStation}}`);
-        }, train.timeToStation * 1000);
-
-      } else if (train.lineName === 'Jubilee') {
-        const note = assignNoteForJubileeLine(train.stationName);
-        setTimeout(() => {
-          instruments.Jubilee.triggerAttackRelease(note, '16n');
-          console.log(`${train.stationName} on the ${train.lineName} Time To Station: ${train.timeToStation}}`);
-        }, train.timeToStation * 1000);
-
-      } else if (train.lineName === 'Northern') {
-        const note = assignNoteForNorthernLine(train.stationName);
-        setTimeout(() => {
-          instruments.Northern.triggerAttackRelease(note, '16n');
-          console.log(`${train.stationName} on the ${train.lineName} Time To Station: ${train.timeToStation}}`);
-        }, train.timeToStation * 1000);
       }
-    })
-  }
+    };
 
-  let dataForVisualisation = localStorage.getItem('quantisedData')
+    fetchData();
+    const interval = setInterval(fetchData, dataBlockDuration * 1000);
+
+    return () => {
+      clearInterval(interval); // clear interval on component unmount
+    };
+  }, []); // Empty dependency array to run only once when the component is rendered
 
   return (
     <div className="App">
-      <DataVisualiser data={dataForVisualisation} />
-      {/* <header className="App-header">
-        <h2>LUSO</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <button id="soundon" onClick={soundOn}>Sound On</button>
-      </header> */}
-      <section>
-        <button id="soundon" onClick={soundOn}>Sound On</button>
-        <button type="button" onClick={() => fadeElement("Perivale")}>Central Fade</button>
-        <TubeMap/>
-      </section>
+      <DataVisualiser data={quantisedData} />
     </div>
   );
 }
--- frontend/src/components/DataVisualiser/DataVisualiser.js
+++ frontend/src/components/DataVisualiser/DataVisualiser.js
@@ -5,76 +5,107 @@ const DataVisualiser = ({ data }) => {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const labels = [];
-    const lineNames = [];
-    const intervals = {};
+    let chartInstance = null;
 
-    // Extract data for chart
-    data.forEach((item) => {
-      const { lineName, timeToStation } = item;
+    const createChart = () => {
+      const labels = [];
+      const lineNames = [];
+      const intervals = {};
 
-      if (!lineNames.includes(lineName)) {
-        lineNames.push(lineName);
-      }
+      const colorMap = {
+        Bakerloo: 'rgba(139, 69, 19, 0.7)', // Brown color for Bakerloo
+        Central: 'rgba(220, 36, 31, 0.7)',
+        Circle: 'rgba(255, 206, 67, 0.7)',
+        District: 'rgba(0, 125, 50, 0.7)',
+        'Hammersmith&City': 'rgba(255, 105, 180, 0.7)', // Pink color with opacity for Hammersmith & City
+        Jubilee: 'rgba(190, 190, 190, 0.7)', // Light grey/silver color with opacity for Jubilee
+        Metropolitan: 'rgba(155, 52, 102, 0.7)',
+        Northern: 'rgba(0, 0, 0, 0.7)',
+        Piccadilly: 'rgba(0, 25, 168, 0.7)',
+        Victoria: 'rgba(0, 152, 216, 0.7)',
+        'Waterloo&City': 'rgba(82, 179, 217, 0.7)',
+      };
 
-      if (!labels.includes(timeToStation)) {
-        labels.push(timeToStation);
-      }
+      // Extract data for chart
+      data.forEach((item) => {
+        const { lineName, timeToStation } = item;
 
-      if (!intervals[lineName]) {
-        intervals[lineName] = {};
-      }
+        if (!lineNames.includes(lineName)) {
+          lineNames.push(lineName);
+        }
 
-      if (!intervals[lineName][timeToStation]) {
-        intervals[lineName][timeToStation] = 0;
-      }
+        const timeInSeconds = Math.floor(timeToStation);
 
-      intervals[lineName][timeToStation]++;
-    });
+        if (!labels.includes(timeInSeconds)) {
+          labels.push(timeInSeconds);
+        }
 
-    // Create chart
-    const chartData = {
-      labels,
-      datasets: lineNames.map((lineName) => ({
-        label: lineName,
-        data: labels.map((timeToStation) =>
-          intervals[lineName][timeToStation] || 0
-        ),
-        backgroundColor: getRandomColor(),
-        borderWidth: 1,
-      })),
-    };
+        if (!intervals[lineName]) {
+          intervals[lineName] = {};
+        }
 
-    const chartOptions = {
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Time to Station',
+        if (!intervals[lineName][timeInSeconds]) {
+          intervals[lineName][timeInSeconds] = 0;
+        }
+
+        intervals[lineName][timeInSeconds]++;
+      });
+
+      // Create chart
+      const chartData = {
+        labels,
+        datasets: lineNames.map((lineName) => ({
+          label: lineName,
+          data: labels.map(
+            (timeInSeconds) => intervals[lineName][timeInSeconds] || 0
+          ),
+          backgroundColor: colorMap[lineName], // Use the assigned color for the lineName
+          borderWidth: 1,
+        })),
+      };
+
+      const chartOptions = {
+        indexAxis: 'x',
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Time (seconds)',
+            },
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Count',
+            },
           },
         },
-        y: {
-          title: {
-            display: true,
-            text: 'Count',
-          },
-        },
-      },
+      };
+
+      const ctx = chartRef.current.getContext('2d');
+
+      // Destroy the previous chart instance if it exists
+      if (chartInstance) {
+        chartInstance.destroy();
+      }
+
+      // Create a new chart instance
+      chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: chartOptions,
+      });
     };
 
-    const ctx = chartRef.current.getContext('2d');
-    new Chart(ctx, {
-      type: 'bar',
-      data: chartData,
-      options: chartOptions,
-    });
+    createChart();
+
+    // Cleanup function
+    return () => {
+      if (chartInstance) {
+        chartInstance.destroy();
+      }
+    };
   }, [data]);
-
-  // Helper function to generate random colors
-  const getRandomColor = () =>
-    `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
-      Math.random() * 256
-    )}, ${Math.floor(Math.random() * 256)}, 0.7)`;
 
   return <canvas ref={chartRef} />;
 };
